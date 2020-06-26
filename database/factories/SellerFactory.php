03193930210a8b42d79435460f7bd89b5d3ab0f6
Abdo <abdo@abdo.com>
2020-06-26 22:40:53 +0200

    prepare models, controllers, resources, factories, migrations, and database

---
+++ database/factories/SellerFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Seller;
+use Faker\Generator as Faker;
+
+$factory->define(Seller::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
